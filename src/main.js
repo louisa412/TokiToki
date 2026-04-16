a4a23801192f0aacf5e7ccdb942d0fd2d7b6e075
louisa412 <louisa412@gmail.com>
2026-04-16 14:04:47 +0800

    update

--- src/main.js
+++ src/main.js
@@ -6,3 +6,10 @@ import './style.css'
 const app = createApp(App)
 app.use(createPinia())
 app.mount('#app')
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', async () => {
+    const regs = await navigator.serviceWorker.getRegistrations()
+    regs.forEach(reg => reg.update())
+  })
+}
