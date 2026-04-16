d4527151354f8def5140444d535920eb6551a5a4
louisa412 <louisa412@gmail.com>
2026-04-16 11:14:48 +0800

    Add files via upload
    init

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'TokiToki',
+        short_name: 'TokiToki',
+        description: '傲嬌電子寵物 Toki',
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'standalone',
+        icons: [
+          { src: '/icon.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icon.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ]
+})
